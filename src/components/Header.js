faca3883e3b1773a0eae52ec8376c96e4261ce67
rawblue-sudo <56031736+rawblue-sudo@users.noreply.github.com>
2019-11-05 17:19:37 +0100

    Revert "Update Header.js"
    This reverts commit 5884aa595804324730f5cc30cc29eba3185a4083.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import _ from 'lodash';
-import { Helmet } from "react-helmet";
 
 import {toStyleObj, safePrefix} from '../utils';
 import Branding from './Branding';
@@ -9,14 +8,10 @@ import Navigation from './Navigation';
 export default class Header extends React.Component {
     render() {
         return (
-          <div className="application">
-        <Helmet>
-        <meta name="google-site-verification" content="oLGiPw0a8uKEJ1gGbvMYclATDEHYHd9LF_3a7sryQiM" />
-        </Helmet>
-      </div>
-              {_.get(this.props, 'img_path') ?
+            <header id="masthead" className="site-header">
+              {_.get(this.props, 'img_path') ? 
               <div id="header-bg" className="site-header-bg" style={toStyleObj('background-image:url(\'' + safePrefix(_.get(this.props, 'img_path')) + '\')')}/>
-               : (_.get(this.props, 'site.siteMetadata.header.bg_img') &&
+               : (_.get(this.props, 'site.siteMetadata.header.bg_img') && 
               <div id="header-bg" className="site-header-bg" style={toStyleObj('background-image:url(\'' + safePrefix(_.get(this.props, 'site.siteMetadata.header.bg_img')) + '\')')}/>
               )}
               <div className="site-header-scroll">
